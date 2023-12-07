3bc9ea7a1bad97bb94afcf3d12e8e9a4ca025a66
narges-dpr <nargesdeypir@gmail.com>
2023-12-07 12:48:14 +0330

    fix loadContributorsBackground to show data from callback

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -7,20 +7,25 @@ import retrofit2.Response
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
-fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
+fun loadContributorsCallbacks(
+    service: GitHubService,
+    req: RequestData,
+    updateResults: (List<User>) -> Unit) {
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
         val allUsers = mutableListOf<User>()
         for (repo in repos) {
-            service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
+            service.getRepoContributorsCall(req.org, repo.name)
+                .onResponse { responseUsers ->
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
                 allUsers += users
+                updateResults(allUsers.aggregate())
             }
         }
         // TODO: Why this code doesn't work? How to fix that?
-        updateResults(allUsers.aggregate())
+
     }
 }
 
