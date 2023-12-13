ebd49e04644a84343e3a1632eafb70116bae5621
narges-dpr <nargesdeypir@gmail.com>
2023-12-13 20:22:27 +0330

    coroutine testing -- unknown process

--- test/tasks/Request5ConcurrentKtTest.kt
+++ test/tasks/Request5ConcurrentKtTest.kt
@@ -4,28 +4,22 @@ import contributors.MockGithubService
 import contributors.expectedConcurrentResults
 import contributors.testRequestData
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.Assert
 import org.junit.Test
 
 class Request5ConcurrentKtTest {
     @Test
-    fun testConcurrent() = runBlocking {
+    fun testConcurrent() = runTest {
         val startTime = System.currentTimeMillis()
         val result = loadContributorsConcurrent(MockGithubService, testRequestData)
         Assert.assertEquals("Wrong result for 'loadContributorsConcurrent'", expectedConcurrentResults.users, result)
         val totalTime = System.currentTimeMillis() - startTime
-        /*
-        // TODO: uncomment this assertion
+
         Assert.assertEquals(
             "The calls run concurrently, so the total virtual time should be 2200 ms: " +
                     "1000 ms for repos request plus max(1000, 1200, 800) = 1200 ms for concurrent contributors requests)",
             expectedConcurrentResults.timeFromStart, totalTime
-        )
-        */
-        Assert.assertTrue(
-            "The calls run concurrently, so the total virtual time should be 2200 ms: " +
-                    "1000 ms for repos request plus max(1000, 1200, 800) = 1200 ms for concurrent contributors requests)",
-            totalTime in expectedConcurrentResults.timeFromStart..(expectedConcurrentResults.timeFromStart + 500)
         )
     }
 }
